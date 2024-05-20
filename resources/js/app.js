ed3356351a68f1f5c1e81dceb0bdc27f5b04f31b
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-05-20 21:25:58 +0700

    feat: creating reusable hamburger componnet, responsive navbar component

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,12 +9,14 @@ import DefaultLayout from '@/layouts/DefaultLayouts.vue'
 import NavbarComponent from '@/components/navbar/NavbarComponent.vue'
 import NavItem from '@/components/navbar/NavItem.vue'
 import App from '@/components/App.vue'
+import ButtonHamburger from '@/components/base/ButtonHamburger.vue';
 // ? Initial configuration & Global Registration Component
 const app = createApp()
 app.component('App', App)
     .component('DefaultLayout', DefaultLayout)
     .component('NavbarComponent', NavbarComponent)
     .component('NavItem', NavItem)
+    .component('ButtonHamburger', ButtonHamburger)
     .use(router)
     .use(createPinia())
     .mount('#app')
--- resources/js/components/navbar/NavbarComponent.vue
+++ resources/js/components/navbar/NavbarComponent.vue
@@ -1,75 +1,33 @@
 <!-- ? Sample Navbar Components -->
 <script setup>
-
+import { ref } from 'vue'
+const hamburger = ref(true)
 </script>
 <template>
     <nav class="bg-white border-gray-200">
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
             <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="../../../public/Logo.svg" class="size-16" alt="Flowbite Logo" />
+                <img src="../../../public/Logo.svg" class="size-16" alt="Open your mind" />
             </a>
             <div class="flex md:order-2">
-                <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search"
-                    aria-expanded="false"
-                    class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                    <span class="sr-only">Search</span>
-                </button>
+                <ButtonHamburger @hamburgerToggle="hamburger = !hamburger" />
                 <div class="relative hidden md:block">
-                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
+                    <!-- <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                         <span class="sr-only">Search icon</span>
-                    </div>
-                    <input type="text" id="search-navbar"
+                    </div> -->
+                    <!-- <input type="text" id="search-navbar"
                         class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search...">
+                        placeholder="Search..."> -->
                 </div>
-                <div class="relative hidden md:block">
-                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                        <span class="sr-only">Search icon</span>
-                    </div>
-                    <input type="text" id="search-navbar"
-                        class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search...">
-                </div>
-                <button data-collapse-toggle="navbar-search" type="button"
-                    class="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-search" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
             </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
-                <div class="relative mt-3 md:hidden">
-                    <div class="absolute inset-y-0 flex items-center pointer-events-none start-0 ps-3">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="text" id="search-navbar"
-                        class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Search...">
-                </div>
-                <NavItem />
+            <div :class="[hamburger ? 'items-center justify-between hidden w-full md:flex md:w-auto md:order-1' : 'items-center justify-between block w-full md:flex md:w-auto md:order-1']"
+                id="navbar-search">
+                <NavItem :hamburger="hamburger" />
             </div>
         </div>
     </nav>
