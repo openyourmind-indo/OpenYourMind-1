46bf5dc5b412b4b4e0dac430462707191a954771
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-05-20 19:22:17 +0700

    routing dasar

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,49 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/services/{serviceID}',function(){
+
+})
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/article', function () {
+    return view('register');
+});
+
+Route::get('/article/{article-id}', function () {
+    return view('register');
+});
+
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+
+
+
+
+
+
+
