cac0ec9f9c3241d73de090a8a2a9bba544cf1ba6
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-06-06 00:09:59 +0700

    feat: create a reusable button component

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,7 @@ import NavbarComponent from '@/components/navbar/NavbarComponent.vue'
 import NavItem from '@/components/navbar/NavItem.vue'
 import App from '@/components/App.vue'
 import ButtonHamburger from '@/components/base/ButtonHamburger.vue';
+import Button from '@/components/base/ButtonComponent.vue';
 // ? Initial configuration & Global Registration Component
 const app = createApp()
 app.component('App', App)
@@ -17,6 +18,7 @@ app.component('App', App)
     .component('NavbarComponent', NavbarComponent)
     .component('NavItem', NavItem)
     .component('ButtonHamburger', ButtonHamburger)
+    .component('Button', Button)
     .use(router)
     .use(createPinia())
     .mount('#app')
---
+++ resources/js/components/base/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineEmits(['actions'])
+defineProps({
+    title: {
+        type: String,
+        required: true,
+        default: 'Contact Us'
+    }, class: {
+        type: String,
+        // ? is relate with responsive button
+        default: 'text-red-600'
+    }, disabled: {
+        required: false
+    }
+})
+</script>
+<template>
+    <button :disabled="disabled" @click="$emit('actions')" :class="class">
+        {{ title }}
+        <slot />
+    </button>
+</template>
