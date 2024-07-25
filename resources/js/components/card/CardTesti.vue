8bc62ca9fa3dd47b69bfec2ba9406aef9c86d1ed
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2024-07-25 10:29:45 +0700

    feat: creating component and register component

--- resources/js/components/card/CardTesti.vue
+++ resources/js/components/card/CardTesti.vue
@@ -1,24 +1,27 @@
+<script setup>
+defineProps({
+    data: {
+        type: [Object, Array],
+        required: true
+    }
+})
+</script>
 <template>
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-            <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
-        </a>
-        <div class="p-5">
-            <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology
-                    acquisitions 2021</h5>
+    <div class="grid grid-cols-1 lg:grid-cols-2 place-items-center place-content-center gap-x-1">
+        <div v-for="({ title, description, img, id }) in data" :key="id"
+            class="grid max-w-sm mb-4 text-center shadow lg:mb-0 rounded-2xl bg-primary-main place-items-center place-content-center dark:bg-gray-800 dark:border-gray-700">
+            <a href="#" class="py-6">
+                <img class="rounded-full place-self-center size-24" :src="img" :alt="title" />
             </a>
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
-                acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
-            </a>
+            <div class="p-5">
+                <p class="mb-3 font-normal text-white font-main">
+                    Open your mind adalah salah satu salah satu, yang terbaik untuk memberikan sebuah sebuah contoh dumy
+                    text. kita berharap bisa menjadi open your mind.
+                </p>
+                <p class="text-lg text-white font-secondary">
+                    {{ title }}
+                </p>
+            </div>
         </div>
     </div>
 </template>
